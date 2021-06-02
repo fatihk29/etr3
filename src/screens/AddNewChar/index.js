9bba0995aaecc5b0cdc7c9178edc64f00de3f2e9
fatihkrc7@gmail.com <fatihkrc7@gmail.com>
2021-06-02 23:49:58 +0300

    textinput added

--- src/screens/AddNewChar/index.js
+++ src/screens/AddNewChar/index.js
@@ -1,27 +1,54 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import styles from './style';
 
 function AddNewChar() {
   const [enteredText, setEnteredText] = useState({
     name: '',
-    avatar: '',
     job: '',
     about: '',
+    avatar: '',
   });
+
+  function onPressAddNewCharBtn() {
+    console.log('14AA', 'pressed');
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Name Surname</Text>
+      <Text>Name Surname:</Text>
       <TextInput
-        placeholder="Course Goal"
-        style={styles.input}
+        placeholder="Name"
+        style={styles.textinputName}
         onChangeText={text => setEnteredText({...enteredText, name: text})}
         value={enteredText.name}
       />
-      <Text>Job</Text>
+      <Text>Job:</Text>
+      <TextInput
+        placeholder="Job"
+        style={styles.textinputJob}
+        onChangeText={text => setEnteredText({...enteredText, job: text})}
+        value={enteredText.job}
+      />
       <Text>About</Text>
+      <TextInput
+        placeholder="About"
+        style={styles.textinputAbout}
+        onChangeText={text => setEnteredText({...enteredText, about: text})}
+        value={enteredText.about}
+      />
       <Text>Link</Text>
+      <TextInput
+        placeholder="Link"
+        style={styles.textinputLink}
+        onChangeText={text => setEnteredText({...enteredText, avatar: text})}
+        value={enteredText.avatar}
+      />
+      <TouchableOpacity
+        onPress={onPressAddNewCharBtn}
+        style={styles.addNewCharBtn}>
+        <Text style={styles.addNewCharBtnText}>Add New Character</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/AddNewChar/style.js
+++ src/screens/AddNewChar/style.js
@@ -1,9 +1,53 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+
+const {width} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    marginTop: 10,
+    marginLeft: 10,
+  },
+  textinputName: {
+    width: width - 20,
+    height: 50,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  textinputJob: {
+    width: width - 20,
+    height: 50,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  textinputAbout: {
+    width: width - 20,
+    height: 50,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  textinputLink: {
+    width: width - 20,
+    height: 50,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  addNewCharBtn: {
+    width: width - 20,
+    height: 35,
+    marginVertical: 20,
+    borderRadius: 10,
+    backgroundColor: '#6495ED',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addNewCharBtnText: {
+    fontSize: 20,
   },
 });
 export default styles;

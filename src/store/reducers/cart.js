751be8e6709eb74ccf0b00ebf4b4644ca11d0258
fatihkrc7@gmail.com <fatihkrc7@gmail.com>
2021-06-05 22:32:43 +0300

    updated

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -9,25 +9,35 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_NEW_CHAR:
       const addedItem = action.product;
-      const prodPrice = addedItem.name;
-      const prodTitle = addedItem.title;
+      const prodID = addedItem.id;
+      const prodName = addedItem.name;
+      const prodJob = addedItem.job;
+      const prodAvatar = addedItem.avatar;
+      const prodLink = addedItem.link;
 
-      let updatedOrNewCartItem;
+      let updatedOrNewItem;
 
-      if (state.items[addedITem.id]) {
-        updatedOrNewCartItem = new Item(
-          state.items[addedITem.id].quantity + 1,
-          prodPrice,
-          prodTitle,
-          state.items[addedITem.id].sum + prodPrice,
+      if (state.items[addedItem.id]) {
+        updatedOrNewItem = new Item(
+          state.items[addedItem.id].quantity + 1,
+          prodName,
+          prodJob,
+          prodAvatar,
+          prodLink,
+          state.items[addedItem.id],
         );
       } else {
-        updatedOrNewCartItem = new Item(1, prodPrice, prodTitle, prodPrice);
+        updatedOrNewItem = new Item(
+          prodID,
+          prodName,
+          prodJob,
+          prodAvatar,
+          prodLink,
+        );
       }
       return {
         ...state,
-        items: {...state.items, [addedITem.id]: updatedOrNewCartItem},
-        totalAmount: state.totalAmount + prodPrice,
+        items: {...state.items, [addedItem.id]: updatedOrNewItem},
       };
   }
   return state;
